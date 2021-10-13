0033f1000638cf8d2282b479d76519a9a48a192b
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-10-13 13:51:51 -0500

    Comments component creation and inclusion

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content'
+import CommentsList from '../components/CommentsList';
 
 
 const ArticlePage = ({ match }) => { 
@@ -36,6 +37,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles</h3>
       <ArticlesList articles={otherArticles} />
     </>
